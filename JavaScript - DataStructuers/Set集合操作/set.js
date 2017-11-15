3c2b7ae72259c41d8e30c00e8f24f2fc79f8e560
linshuangqiang <31532248+linshuangqiang@users.noreply.github.com>
2017-11-15 17:59:11 +0800

    JavaScript-DataStructures

--- JavaScript - DataStructuers/Set集合操作/set.js
+++ JavaScript - DataStructuers/Set集合操作/set.js
@@ -7,11 +7,11 @@ function Set(){
     this.has = function(value){
         return value in items;
         //return items.hasOwnProperty(value);
-    }
+    };
     //向集合中添加元素
     this.add = function(value){
         items[value] = value;
-    }
+    };
     //从集合中删除元素
     this.remove = function(value){
         if(this.has(value)){
@@ -19,15 +19,15 @@ function Set(){
             return true;
         }
         return false;
-    }
+    };
     //清空集合
     this.clear = function(){
         items = {};
-    }
+    };
     //获取集合的长度
     this.size = function(){
         return Object.keys(items).length;
-    }
+    };
     this.sizeLegacy = function() {
         var count = 0;
         for(var prop in items){
@@ -36,11 +36,11 @@ function Set(){
             }
         }
         return count;
-    }
+    };
     //得到集合中的值，结果为数组形式
     this.values = function(){
         return Object.keys(items);
-    }
+    };
 
     //集合的高级操作:4个
     //1。并集  返回包含在两个集合中的所有元素的一个新集合
@@ -55,7 +55,7 @@ function Set(){
             unionSet.add(values[i]);
         }
         return unionSet;
-    }
+    };
     //2.交集  返回包含在两个集合中的元素的新集合
     this.insersection = function(otherSet){
         var insersectionSet = new Set();
@@ -66,7 +66,7 @@ function Set(){
             }
         }
         return insersectionSet;
-    }
+    };
     //3.差集  返回一个存在于第一个  并且不存在于第二个集合中的元素的一个新的集合
     this.difference = function(otherSet){
         var differenceSet = new Set();
@@ -77,7 +77,7 @@ function Set(){
             }
         }
         return differenceSet;
-    }
+    };
     //4.子集  验证一个集合是不是另一个集合的子集
     this.subset = function(otherSet) {
         if(this.size()>otherSet.size()){
@@ -91,5 +91,5 @@ function Set(){
             }
         }
         return true;
-    }
+    };
 }
